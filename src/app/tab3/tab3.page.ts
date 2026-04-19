86a178e7b6ca557f0a22369b0a46bfb32419d8e8
hahaha135135 <623881354@qq.com>
2026-04-19 17:05:17 +0800

    Add files via upload
    setup edit-delete in tab3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent,
+  IonSearchbar, IonList, IonItem, IonInput, IonButton
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  templateUrl: './tab3.page.html',
+  standalone: true,
+  imports: [
+    CommonModule, FormsModule,
+    IonHeader, IonToolbar, IonTitle, IonContent,
+    IonSearchbar, IonList, IonItem, IonInput, IonButton
+  ]
 })
 export class Tab3Page {
-  constructor() {}
+  name = '';
 }
