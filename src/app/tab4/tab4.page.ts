36213942cd1ba8336d7383b76f1c108efea86089
yuanyizheng191-code <yuanyizheng191@gmail.com>
2026-04-19 16:05:23 +0800

    Add files via upload
    Generate and fill in privacy page code

---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: './tab4.page.html',
+  styleUrls: ['./tab4.page.scss'],
+  standalone: true,
+  imports: [CommonModule, IonicModule]
+})
+export class Tab4Page {
+  constructor() {}
+}
