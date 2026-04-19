9177a607598baa7502d9f597f3a8bbc8212b528e
hahaha135135 <623881354@qq.com>
2026-04-19 17:04:46 +0800

    Add files via upload
    Added add-item feature to tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,79 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent,
+  IonList, IonItem, IonInput, IonButton,
+  ToastController
+} from '@ionic/angular/standalone';
+import { InventoryService } from '../services/inventory.service';
 
 @Component({
   selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  templateUrl: './tab2.page.html',
+  standalone: true,
+  imports: [
+    CommonModule, FormsModule,
+    IonHeader, IonToolbar, IonTitle, IonContent,
+    IonList, IonItem, IonInput, IonButton
+  ]
 })
 export class Tab2Page {
+  item_name = '';
+  category = '';
+  quantity = 0;
+  price = 0;
+  supplier_name = '';
+  stock_status = '';
+  featured_item = 0;
+  special_note = '';
 
-  constructor() {}
+  constructor(
+    private service: InventoryService,
+    private toastCtrl: ToastController
+  ) {}
 
+  async addItem() {
+    const data = {
+      item_name: this.item_name,
+      category: this.category,
+      quantity: this.quantity,
+      price: this.price,
+      supplier_name: this.supplier_name,
+      stock_status: this.stock_status,
+      featured_item: this.featured_item,
+      special_note: this.special_note
+    };
+
+    this.service.addItem(data).subscribe({
+      next: async () => {
+        const toast = await this.toastCtrl.create({
+          message: 'Item added successfully',
+          duration: 2000,
+          color: 'success'
+        });
+        toast.present();
+        this.clearForm();
+      },
+      error: async () => {
+        const toast = await this.toastCtrl.create({
+          message: 'Failed to add item',
+          duration: 2000,
+          color: 'danger'
+        });
+        toast.present();
+      }
+    });
+  }
+
+  clearForm() {
+    this.item_name = '';
+    this.category = '';
+    this.quantity = 0;
+    this.price = 0;
+    this.supplier_name = '';
+    this.stock_status = '';
+    this.featured_item = 0;
+    this.special_note = '';
+  }
 }
