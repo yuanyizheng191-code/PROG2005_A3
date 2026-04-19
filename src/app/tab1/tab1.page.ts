9a660cfc3ed589e28cf2e596fa37e717f2c8f4e7
LLJJLL111 <3472746945@qq.com>
2026-04-19 16:18:10 +0800

    Add files via upload
    Integrated inventory list & search into tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent,
+  IonSearchbar, IonList, IonItem, IonLabel,
+  IonSpinner, IonNote
+} from '@ionic/angular/standalone';
+import { InventoryService } from '../services/inventory.service';
+import { InventoryItem } from '../models/inventory.model';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  templateUrl: './tab1.page.html',
+  standalone: true,
+  imports: [
+    CommonModule, FormsModule,
+    IonHeader, IonToolbar, IonTitle, IonContent,
+    IonSearchbar, IonList, IonItem, IonLabel,
+    IonSpinner, IonNote
+  ]
 })
-export class Tab1Page {
-  constructor() {}
+export class Tab1Page implements OnInit {
+  inventoryList: InventoryItem[] = [];
+  searchTerm = '';
+  filteredList: InventoryItem[] = [];
+  loading = true;
+
+  constructor(private service: InventoryService) {}
+
+  ngOnInit() {
+    this.service.getAllItems().subscribe({
+      next: data => {
+        this.inventoryList = data;
+        this.filteredList = [...data];
+        this.loading = false;
+      },
+      error: err => {
+        console.error(err);
+        this.loading = false;
+      }
+    });
+  }
+
+  search() {
+    const term = this.searchTerm.toLowerCase().trim();
+    if (!term) {
+      this.filteredList = [...this.inventoryList];
+      return;
+    }
+    this.filteredList = this.inventoryList.filter(item =>
+      item.item_name.toLowerCase().includes(term)
+    );
+  }
 }
