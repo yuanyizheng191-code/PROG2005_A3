9541f137550c1ecc903e6e172b633da9cc4d8e6b
yuanyizheng191-code <yuanyizheng191@gmail.com>
2026-04-22 23:35:26 +0800

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule,
-    IonApp,
-    IonRouterOutlet
-  ],
+  styleUrls: ['app.component.scss'],
+  standalone: false,
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {}
+}
